24378751ff25082ef4326158070fa9396b9e806b
Kohichi Aoki <drikin@gmail.com>
2026-06-15 10:13:13 -0700

    fix: accept all symbol/emoji chars in buffer (no auto-convert)

--- Sources/OneByte/OneByteInputController.swift
+++ Sources/OneByte/OneByteInputController.swift
@@ -117,7 +117,7 @@ nonisolated public final class OneByteInputController: IMKInputController, @unch
             if !fullText.isEmpty { commitAsIs(client: client) }
             return true
         }
-        let accepted = CharacterSet.lowercaseLetters.union(.uppercaseLetters).union(CharacterSet(charactersIn: " ,.!?'-")).union(.decimalDigits).union(.alphanumerics).union(CharacterSet(charactersIn: "１２３４５６７８９０ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ　！＂＃＄％＆＇（）＊＋，−．／：；＜＝＞？＠［＼］＾＿｀｛｜｝～"))
+        let accepted = CharacterSet(charactersIn: "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 ,.!?\'\"-:;@#$%^&*()_+=[]{}|\\/~`<>　１２３４５６７８９０ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ”！＃＄％＆＇（）＊＋，−．／：；＜＝＞？＠［＼］＾＿｀｛｜｝～")
         guard chars.rangeOfCharacter(from: accepted.inverted) == nil else {
             if !fullText.isEmpty { doConvert(client: client, mode: .toJapanese) }
             return false
